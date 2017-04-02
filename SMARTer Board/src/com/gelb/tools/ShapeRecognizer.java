776ce6330e20d2cbb37f8fcd138b040299b16e9b
Mika <Mint2017.Gelb@UERHE11WS01.hpi.uni-potsdam.de>
2017-04-02 14:10:59 +0200

    Improved Shape recognition

--- SMARTer Board/src/com/gelb/tools/ShapeRecognizer.java
+++ SMARTer Board/src/com/gelb/tools/ShapeRecognizer.java
@@ -6,29 +6,35 @@ import java.util.List;
 
 public class ShapeRecognizer {
 
-	public static Rectangle2D.Double getRectangle(List<Point2D.Double> points){
+	public static Rectangle2D.Double getRectangle(List<Point2D.Double> points) {
 		double smallestX = Double.POSITIVE_INFINITY, smallestY = Double.POSITIVE_INFINITY, biggestX = 0, biggestY = 0;
-		for(Point2D.Double point:points) {
-			if(point.getX()>biggestX)
-				biggestX=point.getX();
-			else if(point.getX()<smallestX)
-				smallestX=point.getX();
-			if(point.getY()>biggestY)
-				biggestY=point.getY();
-			else if(point.getY()<smallestY)
-				smallestY=point.getY();
+		for (Point2D.Double point : points) {
+			if (point.getX() > biggestX)
+				biggestX = point.getX();
+			else if (point.getX() < smallestX)
+				smallestX = point.getX();
+			if (point.getY() > biggestY)
+				biggestY = point.getY();
+			else if (point.getY() < smallestY)
+				smallestY = point.getY();
 		}
-		Rectangle2D.Double outerRect=new Rectangle2D.Double(smallestX, smallestY, biggestX-smallestX, biggestY-smallestY);
-		double diff=Math.sqrt(outerRect.width*outerRect.width+outerRect.height+outerRect.height)/10;
-		Rectangle2D.Double innerRect=new Rectangle2D.Double(smallestX+diff, smallestY+diff, biggestX-smallestX-2*diff, biggestY-smallestY-2*diff);
-		int pointsInside = 0;
-		for(Point2D.Double point:points) {
-			if(outerRect.contains(point)&&!innerRect.contains(point))
-				pointsInside++;
-		}
-		if((double)pointsInside/points.size()>=0.5)
-			return outerRect;
-		else
+		Rectangle2D.Double outerRect = new Rectangle2D.Double(smallestX, smallestY, biggestX - smallestX,
+				biggestY - smallestY);
+		double diff = Math.sqrt(outerRect.width * outerRect.width + outerRect.height + outerRect.height) / 8;
+		if (points.get(0).distance(points.get(points.size() - 1)) > diff)
 			return null;
+		else {
+			Rectangle2D.Double innerRect = new Rectangle2D.Double(smallestX + diff, smallestY + diff,
+					biggestX - smallestX - 2 * diff, biggestY - smallestY - 2 * diff);
+			int pointsInside = 0;
+			for (Point2D.Double point : points) {
+				if (outerRect.contains(point) && !innerRect.contains(point))
+					pointsInside++;
+			}
+			if ((double) pointsInside / points.size() >= 0.8)
+				return outerRect;
+			else
+				return null;
+		}
 	}
 }
