11f68b2465aee204d8f7c673810fd5aa4755f66a
Mika <Mint2017.Gelb@UERHE13WS09.hpi.uni-potsdam.de>
2017-04-02 10:56:27 +0200

    undo/redo added

--- SMARTer Board/src/com/gelb/smarterboard/Tafel.java
+++ SMARTer Board/src/com/gelb/smarterboard/Tafel.java
@@ -37,7 +37,7 @@ public class Tafel {
 		try {
 			int width = (int) mCanvas.getWidth();
 			int height = (int) mCanvas.getHeight();
-			BufferedImage bi = new BufferedImage(width,height,BufferedImage.TYPE_INT_ARGB);
+			BufferedImage bi = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
 			WritableImage writableImage = new WritableImage(width, height);
 			mCanvas.snapshot(null, writableImage);
 			SwingFXUtils.fromFXImage(writableImage, bi);
@@ -51,7 +51,7 @@ public class Tafel {
 	}
 
 	public void save(File file) {
-		File temp=new File(file.getParentFile(), file.getName()+"_temp");
+		File temp = new File(file.getParentFile(), file.getName() + "_temp");
 		saveToFolder(temp);
 		ZipAPI.zipFolder(temp, file);
 		ZipAPI.deleteFolder(temp);
@@ -61,7 +61,7 @@ public class Tafel {
 		try {
 			BufferedImage img = ImageIO.read(new File(folder, "image.png"));
 			Canvas c = new Canvas(img.getWidth(), img.getHeight());
-			c.getGraphicsContext2D().drawImage(SwingFXUtils.toFXImage(img, null ), 0, 0);
+			c.getGraphicsContext2D().drawImage(SwingFXUtils.toFXImage(img, null), 0, 0);
 			JSONObject json = JSONFileAPI.load(new File(folder, "structure.json"));
 			Color backgroundColor = JSONFileAPI.getColor(json.getJSONObject("background_color"));
 			return new Tafel(c, backgroundColor);
@@ -72,10 +72,11 @@ public class Tafel {
 		}
 
 	}
+
 	public static Tafel load(File file) {
-		File temp=new File(file.getParentFile(), file.getName()+"_temp");
+		File temp = new File(file.getParentFile(), file.getName() + "_temp");
 		ZipAPI.unzipFolder(file, temp);
-		Tafel tafel=loadFromFolder(temp);
+		Tafel tafel = loadFromFolder(temp);
 		ZipAPI.deleteFolder(temp);
 		return tafel;
 	}
@@ -92,35 +93,52 @@ public class Tafel {
 		return mCanvas;
 	}
 
-	//TafelHistory
+	// TafelHistory
 	private int mHistorySize = 10;
 	private String mHistoryDir = Paths.get(".").toAbsolutePath().normalize().toString() + "/test.history";
 	private LinkedList<File> mHistoryFiles = new LinkedList<>();
 
-	public void setHistorySize(int size){
+	public void setHistorySize(int size) {
 		mHistorySize = size;
 	}
 
 	private int historyCount = 0;
 
-	public void addToHistory(){
+	public void addToHistory() {
 		File savingFile = new File(mHistoryDir + "/" + historyCount + ".sb");
 		save(savingFile);
 
 		mHistoryFiles.add(savingFile);
-		if(mHistoryFiles.size() > mHistorySize){
+		if (mHistoryFiles.size() > mHistorySize) {
 			mHistoryFiles.get(0).delete();
 			mHistoryFiles.pollFirst();
 		}
 		historyCount++;
 	}
 
-	public void redo(){
-
+	public boolean hasRedo() {
+		return new File(mHistoryDir + "/" + (historyCount + 1) + ".sb").exists();
 	}
 
-	public void undo(){
-
+	public boolean hasUndo() {
+		return new File(mHistoryDir + "/" + (historyCount - 1) + ".sb").exists();
 	}
 
+	public Tafel getRedo() throws Exception {
+		if (hasRedo()) {
+			File savingFile = new File(mHistoryDir + "/" + (historyCount + 1) + ".sb");
+			historyCount++;
+			return load(savingFile);
+		}
+		throw new Exception("History is not that long");
+	}
+
+	public Tafel getUndo() throws Exception{
+		if (hasUndo()) {
+			File savingFile = new File(mHistoryDir + "/" + (historyCount - 1) + ".sb");
+			historyCount++;
+			return load(savingFile);
+		}
+		throw new Exception("History is not that long");
+	}
 }
